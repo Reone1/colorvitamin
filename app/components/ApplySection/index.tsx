dcb4cc0cb04401f3a4e69f11cd9ccc33c9535401
Re1one <j901207@gmail.com>
2020-02-13 14:01:08 +0900

    update: google form link target

--- app/components/ApplySection/index.tsx
+++ app/components/ApplySection/index.tsx
@@ -8,7 +8,7 @@ const applyGogggleFormPath = 'https://forms.gle/Yne1Fv4zGHTFHMAW7';
 export default () => {
     return(
     <Wrapper>
-        <Button href={`${applyGogggleFormPath}`}>신 청 하 기</Button>
+        <Button target={`_blank`} href={`${applyGogggleFormPath}`}>신 청 하 기</Button>
     </Wrapper>
     );
 };
